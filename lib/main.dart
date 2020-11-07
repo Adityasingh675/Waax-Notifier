aec344c39f580d49a5279c12f796f06d529b7634
Adisingh675 <adisingh675@gmail.com>
2020-11-07 19:52:27 +0530

    major changes implementing streamProvider.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wax_notifier_app/provider/settings_provider.dart';
 import 'package:wax_notifier_app/screens/home.dart';
+import 'package:wax_notifier_app/services/firestore_service.dart';
 
-void main() => runApp(MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: ((BuildContext context) => SettingsProvider()),
+    final FirestoreService _db = FirestoreService();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (BuildContext context) => SettingsProvider(),),
+        StreamProvider(create: (BuildContext context) => _db.getReports(),)
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,20 @@
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
+import 'package:wax_notifier_app/models/report.dart';
+import 'package:wax_notifier_app/provider/settings_provider.dart';
 import 'package:wax_notifier_app/screens/settings.dart';
+import 'package:provider/provider.dart';
+import 'package:wax_notifier_app/services/firestore_service.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SettingsProvider settings = Provider.of<SettingsProvider>(context);
+    var reports = Provider.of<List<Report>>(context)
+        .where((report) => settings.waxLines.contains(report.line))
+        .toList();
+    FirestoreService _db = FirestoreService();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Wax-Noty App"),
@@ -16,6 +27,27 @@ class Home extends StatelessWidget {
             },
           )
         ],
+      ),
+      body: ListView.builder(
+        itemCount: reports?.length ?? 0,
+        itemBuilder: (context, index) {
+          Report report = reports[index];
+          return ListTile(
+            leading: (settings.units == 'Metric')
+                ? Text(report.temp.toString())
+                : Text((((report.temp) * (9 / 5)) + 32).round().toString() + '\u00B0'),
+            title: Text(report.wax),
+            subtitle: Text(report.line),
+            trailing: Text(formatDate(
+                DateTime.parse(report.timestamp), [h, ':', nn, ' ', am])),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          _db.addReport();
+        },
       ),
     );
   }
